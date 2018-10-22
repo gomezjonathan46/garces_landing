7d15a8fb8649b8d8c70044cdfd56506fc6d1a5e6
gomezjonathan46 <gomez.e.jonathan@gmail.com>
2018-10-22 14:50:45 -0400

    Update counter as carousel slides

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// $('.carousel').carousel({
+//     interval: false
+// });
+
+const test1 = document.querySelector('.count')
+const test2 = document.querySelector('.test')
+
+// function count(num) {
+//   if (num.target.classList.contains('active')) {
+//
+//   }
+// }
+
+if (test1.target.classList.contains('active')) {
+  test2.classList.toggle('test')
+}
